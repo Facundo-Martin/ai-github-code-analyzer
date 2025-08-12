af3a78faa50c3835b19bd2bf4c79e5565633f952
Facundo Martin <facundomartin98@gmail.com>
2025-08-12 11:35:10 -0300

    style: create app sidebar

---
+++ src/app/_components/app-sidebar.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "~/lib/utils";
+
+import {
+  Bot,
+  CreditCard,
+  LayoutDashboard,
+  Plus,
+  Presentation,
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenu,
+  useSidebar,
+} from "~/components/ui/sidebar";
+import { Button } from "~/components/ui/button";
+import Image from "next/image";
+
+const NAVIGATION_ITEMS = [
+  { title: "Dashboard", url: "/dashboard", icon: LayoutDashboard },
+  { title: "Q&A", url: "/qa", icon: Bot },
+  { title: "Meetings", url: "/meetings", icon: Presentation },
+  { title: "Billing", url: "/billing", icon: CreditCard },
+] as const;
+
+const DUMMY_PROJECTS = [
+  { name: "Project 1" },
+  { name: "Project 2" },
+  { name: "Project 3" },
+] as const;
+
+export function AppSidebar() {
+  const pathname = usePathname();
+  const { open } = useSidebar();
+
+  return (
+    <Sidebar collapsible="icon" variant="floating">
+      {/* TODO: Create logo */}
+      <SidebarHeader>
+        <div className="flex items-center gap-2">
+          <Image src="" alt="logo" width={40} height={40} />
+          {open && (
+            <h1 className="text-primary/80 text-xl font-bold">Dionysus</h1>
+          )}
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Applications</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {NAVIGATION_ITEMS.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link
+                      href={item.url}
+                      className={cn({
+                        "!bg-primary !text-white": pathname === item.url,
+                      })}
+                    >
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Your Projects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {DUMMY_PROJECTS.map((project) => (
+                <SidebarMenuItem key={project.name}>
+                  <SidebarMenuButton asChild>
+                    <div>
+                      {/* TODO: Update this once I have created my db model */}
+                      <div
+                        className={cn(
+                          "text-primary flex size-6 items-center justify-center rounded-sm border bg-white text-sm",
+                          {
+                            "bg-primary text-white": false,
+                          },
+                        )}
+                      >
+                        {project.name[0]}
+                      </div>
+                      <span>{project.name}</span>
+                    </div>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+              <SidebarMenuItem>
+                <Link href="/create">
+                  <Button
+                    size="sm"
+                    variant="outline"
+                    className="mt-2 w-fit cursor-pointer"
+                  >
+                    <Plus />
+                    Create project
+                  </Button>
+                </Link>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
