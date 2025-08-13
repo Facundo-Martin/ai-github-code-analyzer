69977b89189eddeb81e71e6732b0a2b0e8e89d9b
Facundo Martin <facundomartin98@gmail.com>
2025-08-13 10:27:59 -0300

    feat: integrate createProject mutation on the FE

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -15,6 +15,8 @@ import {
   FormMessage,
 } from "~/components/ui/form";
 import Image from "next/image";
+import { api } from "~/trpc/react";
+import { toast } from "sonner";
 
 const formSchema = z.object({
   repoUrl: z.url().min(1, {
@@ -27,90 +29,97 @@ const formSchema = z.object({
 });
 
 export default function CreateProjectPage() {
+  const { mutate: createProject, isPending: isCreatingProject } =
+    api.project.create.useMutation();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       repoUrl: "",
+      projectName: "",
+      githubToken: "",
     },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+    createProject(values, {
+      onSuccess: () => {
+        toast.success("Project created successfully");
+        form.reset();
+      },
+      onError: () => {
+        toast.error("Failed to create project");
+      },
+    });
   }
 
   return (
-    <div>
-      <h1>Create project page</h1>
+    <div className="grid h-full place-items-center">
       <div className="flex h-full items-center justify-center gap-12">
         {/* TODO: Add create project image */}
-        <Image src="" alt="hmm" height={56} width={120} />
+        <Image src="/image.png" alt="hmm" height={56} width={120} />
         <div>
-          <div>
-            <h1 className="text-2xl font-semibold">
-              Link your Github Repository
-            </h1>
-            <p className="text-muted-foreground text-sm">
-              Enter the URL of your repository to link it to Dionysus
-            </p>
-          </div>
-          {/* Form goes here */}
-          <div>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-2"
-              >
-                <FormField
-                  control={form.control}
-                  name="projectName"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input required placeholder="Project Name" {...field} />
-                      </FormControl>
+          <h1 className="text-2xl font-semibold">
+            Link your Github Repository
+          </h1>
+          <p className="text-muted-foreground text-sm">
+            Enter the URL of your repository to link it to Dionysus
+          </p>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="mt-4 space-y-2"
+            >
+              <FormField
+                control={form.control}
+                name="projectName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input required placeholder="Project Name" {...field} />
+                    </FormControl>
 
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="repoUrl"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input
-                          required
-                          type="url"
-                          placeholder="Repo URL"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="githubToken"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input
-                          placeholder="Github Token (optional)"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" className="mt-2">
-                  Create Project
-                </Button>
-              </form>
-            </Form>
-          </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="repoUrl"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        required
+                        type="url"
+                        placeholder="Repo URL"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="githubToken"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Github Token (optional)" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="mt-2 cursor-pointer"
+                disabled={isCreatingProject}
+              >
+                Create Project
+              </Button>
+            </form>
+          </Form>
         </div>
       </div>
     </div>
